c6038fa6e5a99dea0d409299052223075087c048
bangdonghyuk <yolo0528@naver.com>
2024-08-03 23:04:21 +0900

    첫번째 커밋

---
+++ src/components/layouts/Header.js
@@ -0,0 +1,11 @@
+//Header.js
+
+function Header() {
+    return (
+      <div className="Header">
+        <h2>Header</h2>
+      </div>
+    );
+  }
+  
+  export default Header;
---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+//Home.js
+
+function Home() {
+  return (
+    <div>
+      <h2>Home Page</h2>
+    </div>
+  );
+}
+
+export default Home;
